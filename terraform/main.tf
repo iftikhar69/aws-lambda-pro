4b8674902f2d4832e40fb5dc700a0798ea88dae0
iftikhar69 <178935770+iftikhar69@users.noreply.github.com>
2025-10-28 20:12:46 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_lambda_function" "demo" {
+  function_name = "lambda-demo"
+  handler       = "src/handler.handler"
+  runtime       = "nodejs18.x"
+  role          = "arn:aws:iam::<your-account-id>:role/<your-role>"
+  filename      = "lambda.zip"
+}
